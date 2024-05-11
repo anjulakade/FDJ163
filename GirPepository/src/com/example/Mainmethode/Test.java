a71c307c348ff578517d6998a0bf417170957a49
Admin <Admin@DESKTOP-1UAFC8K>
2024-05-11 15:23:00 +0530

    third commit

--- GirPepository/src/com/example/Mainmethode/Test.java
+++ GirPepository/src/com/example/Mainmethode/Test.java
@@ -9,22 +9,40 @@ public class Test
 		Student s=new Student();
 		s.setId(111);
 		s.setName("anjali");
-		s.setEmail("anju145");
+		s.setEmail("anju145@gmail.com");
 		s.setAge(24);
 		
 		Student s1=new Student();
-		s1.setId(111);
-		s1.setName("anjali");
-		s1.setEmail("anju145");
-		s1.setAge(24);
+		s1.setId(112);
+		s1.setName("manu");
+		s1.setEmail("manu145@gmail.com");
+		s1.setAge(26);
+		
+		Student s2=new Student();
+		s2.setId(113);
+		s2.setName("anu");
+		s2.setEmail("anu123@gmail.com");
+		s2.setAge(24);
 	System.out.println(s.getId());
 	System.out.println(s.getName());
 	System.out.println(s.getEmail());
 	System.out.println(s.getAge());
+	
+	
+	
 	System.out.println(s1.getId());
 	System.out.println(s1.getName());
 	System.out.println(s1.getEmail());
 	System.out.println(s1.getAge());
+	
+	
+	
+	
+	System.out.println(s2.getId());
+	System.out.println(s2.getName());
+	System.out.println(s2.getEmail());
+	System.out.println(s2.getAge());
+	
 	}
 
 	public static void main(String[] args)
