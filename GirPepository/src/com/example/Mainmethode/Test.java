a419e8e22f89efe30d42e68ce93f0ab6a6d4eba4
Admin <Admin@DESKTOP-1UAFC8K>
2024-05-11 15:15:44 +0530

    second commit

--- GirPepository/src/com/example/Mainmethode/Test.java
+++ GirPepository/src/com/example/Mainmethode/Test.java
@@ -11,10 +11,20 @@ public class Test
 		s.setName("anjali");
 		s.setEmail("anju145");
 		s.setAge(24);
+		
+		Student s1=new Student();
+		s1.setId(111);
+		s1.setName("anjali");
+		s1.setEmail("anju145");
+		s1.setAge(24);
 	System.out.println(s.getId());
 	System.out.println(s.getName());
 	System.out.println(s.getEmail());
 	System.out.println(s.getAge());
+	System.out.println(s1.getId());
+	System.out.println(s1.getName());
+	System.out.println(s1.getEmail());
+	System.out.println(s1.getAge());
 	}
 
 	public static void main(String[] args)
